6d4090b882e8c41308021660098f3bab0c58f7a1
Benn Mapes <benn.mapes@gmail.com>
2013-06-13 14:57:47 -0700

    updated contacts.cs for wp7

--- src/wp7/Contacts.cs
+++ src/wp7/Contacts.cs
@@ -1,15 +1,15 @@
 ﻿/*  
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-	
-	http://www.apache.org/licenses/LICENSE-2.0
-	
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 using Microsoft.Phone.Tasks;
@@ -396,11 +396,16 @@ namespace WPCordovaClassLib.Cordova.Commands
                 return;
             }
 
+            //Deal with case where caller has specified no options
             if (searchParams.options == null)
             {
                 searchParams.options = new SearchOptions();
                 searchParams.options.filter = "";
                 searchParams.options.multiple = true;
+            }
+            else if (searchParams.options.filter == null) // Deal with the case where caller has specified partial options
+            {
+                searchParams.options.filter = "";
             }
 
             DeviceContacts deviceContacts = new DeviceContacts();
@@ -623,7 +628,7 @@ namespace WPCordovaClassLib.Cordova.Commands
             }
             else
             {
-                retVal = string.Format(formatStr,"","","","","","");
+                retVal = string.Format(formatStr, "", "", "", "", "", "");
             }
 
             return "{" + retVal + "}";

57000bff3aa9e367a62e3942879d055283d9971b
Inês Sousa <ines.sousa@outsystems.com>
2019-07-25 14:06:47 +0100

    test: update ContactScreen.ts with all page object

--- os-appium-tests/tests/screenobjects/ContactsScreen.ts
+++ os-appium-tests/tests/screenobjects/ContactsScreen.ts
@@ -2,7 +2,7 @@ import * as AndroidUtils from '../helpers/AndroidUtils';
 import * as Context from '../helpers/Context';
 import * as IOSUtils from '../helpers/IOSUtils';
 
-// SETUP BUTTONS
+// SETUP BUTTONS - ADD CONTACT
 
 export function SetupContactAllParameters(): WebdriverIO.Element {
     return Context.getElemBySelector('#Name1');
@@ -32,6 +32,48 @@ export function SetupContactStrangeFormat(): WebdriverIO.Element {
     return Context.getElemBySelector('#strangeformat');
 }
 
+// SETUP BUTTONS - FIND CONTACT
+
+export function SetupFindFirstName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#testapp_name1');
+}
+
+export function SetupFindLastName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#last1');
+}
+
+export function SetupFindCountryDigit(): WebdriverIO.Element {
+    return Context.getElemBySelector('#phonenumber');
+}
+
+export function SetupFindEmail(): WebdriverIO.Element {
+    return Context.getElemBySelector('#email3_outsystems_com');
+}
+
+export function SetupFindNumber1(): WebdriverIO.Element {
+    return Context.getElemBySelector('#number1');
+}
+
+export function SetupFindContactCreatedOnPhone(): WebdriverIO.Element {
+    return Context.getElemBySelector('#testapp_name3');
+}
+
+export function SetupFindIncompleteFirstName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#name2');
+}
+
+export function SetupFindIncompleteEmail(): WebdriverIO.Element {
+    return Context.getElemBySelector('#email3');
+}
+
+export function SetupNonExistentContact(): WebdriverIO.Element {
+    return Context.getElemBySelector('#nonexistentcontact');
+}
+
+export function getSwitchMultipleContacts(): WebdriverIO.Element {
+    return Context.getElemBySelector('#switchMultipleContacts');
+}
+
 // SCREEN ELEMENTS
 
 export function getTitle(): WebdriverIO.Element {
@@ -54,20 +96,78 @@ export function getFindContactScreen(): WebdriverIO.Element {
     return Context.getElemBySelector('#findContactScreen');
 }
 
-export function getSwitchMultipleContacts(): WebdriverIO.Element {
-    return Context.getElemBySelector('#findContactScreen');
-}
-
 export function getFindContactButton(): WebdriverIO.Element {
     return Context.getElemBySelector('#findContactButton');
 }
 
-    // ********PICK CONTACT SCREEN********/
+export function getFindContactResultList(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l1-0-ListItem1');
+}
 
 export function getPickContactScreen(): WebdriverIO.Element {
-    return $('#pickContactScreen');
+    return Context.getElemBySelector('#pickContactScreen');
 }
 
 export function getPickContactButton(): WebdriverIO.Element {
-    return $('#pickContactButton');
+    return Context.getElemBySelector('#pickContactButton');
+}
+
+// Contacts Validation
+
+export function getValidateFirstName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#firstname');
+}
+
+export function getValidateLastName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#lastName');
+}
+
+export function getValidateDisplayedName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#displayName');
+}
+
+export function getValidatePhoneNumber(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l1-0-phoneNumber');
+}
+
+export function getValidateEmail(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l2-0-Email');
+}
+
+export function getValidateCompanyName(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l5-0-companyName');
+}
+
+export function getValidateCompanyDepartment(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l5-0-companyDepartment');
+}
+
+export function getValidateJobTitle(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l5-0-jobTitle');
+}
+
+export function getValidateAddress(): WebdriverIO.Element {
+    return Context.getElemBySelector('#l3-0-address');
+}
+
+export function getValidateBirthday(): WebdriverIO.Element {
+    return Context.getElemBySelector('#birthday');
+}
+
+export function getValidateNotes(): WebdriverIO.Element {
+    return Context.getElemBySelector('#note');
+}
+
+// Bottom Bar
+
+export function getAddContactBottomMenu(): WebdriverIO.Element {
+    return Context.getElemBySelector('#b4-AddContactBottomBar');
+}
+
+export function getFindContactBottomMenu(): WebdriverIO.Element {
+    return Context.getElemBySelector('#b4-FindContactBottomBar');
+}
+
+export function getPickContactBottomMenu(): WebdriverIO.Element {
+    return Context.getElemBySelector('#b4-PickContactBottomBar');
 }
